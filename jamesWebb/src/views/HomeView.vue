43606d1af08921a583326184c8dc7c9b925f8309
snow-sr <joaofelipi877@gmail.com>
2022-07-12 19:42:21 -0300

    Tailwinds, build in /docs and more

--- jamesWebb/src/views/HomeView.vue
+++ jamesWebb/src/views/HomeView.vue
@@ -1,9 +1,15 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="h-screen max-h-max bg-gray-800 text-white">
+    <div>
+      <!--Title-->
+      <h1 class="text-7xl text-center pt-10 underline">
+        The James Webb Archive
+      </h1>
+    </div>
+    <div>
+      <!--cards with images-->
+    </div>
   </main>
 </template>
